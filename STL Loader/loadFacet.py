ea8cd1e5f4a6ac138759a0d0a40fcaced1ab893f
jelmer04 <jon.s.elmer@googlemail.com>
2014-07-04 08:51:56 +0100

    Binary files added

--- STL Loader/loadFacet.py
+++ STL Loader/loadFacet.py
@@ -3,69 +3,89 @@ Created by Jon Elmer on 01-07-2014
 """
 
 from tkinter import *       # GUI
+import struct               # Byte manipulation
+import os                   # File size
 
-def load_STL_ASCII (fileName, sliceDepth):
-    """Load a facet from an STL file"""
+def load_STL(fileName):
+    """Load the STL file, either ASCII or Binary - detected automatically"""
 
-    pointsList = []
-
-    file = open(fileName, "r")
-
-    line = file.readlines(1)[0]
-    
-    if line.startswith("solid"):
-        #ASCII file
-        print("Looks like an ASCII file")
-        
+    # Open the file for binary reading
+    file = open(fileName, "br")
+    # Skip the header
+    file.seek(80)
+    # Read the 4 byte unsigned long for the number of facets in the solid and convert to unsigned integer
+    facetCount = struct.unpack('<I', file.read(4))[0]
+    # Estimate the size of the file (50 bytes per facet, plus 80 header and 4 facet count)
+    estimateSize = 84 + (facetCount * 50)
+    # Get the number of bytes in the file
+    fileSize = os.path.getsize(fileName)
+    # Print the file size comparison
+    print("File should contain", facetCount, "facets")
+    print("File should be  ", estimateSize, "bytes")
+    print("File is actually", fileSize,"bytes")
+    file.close()
+    # Check the size of the file is as expected for the number of facets
+    if fileSize == estimateSize:
+        # Its a binary file
+        print("Binary file!")
+        file = open(fileName,"br")
+        facetList = load_STL_Binary(file)
+        file.close()
     else:
-        print("Bad File")
+        # It might be an ASCII file
+        print("ASCII or other file!")
+        file = open(fileName,"r")
+        facetList = load_STL_ASCII(file)
+        file.close()
+    return facetList
+            
+# End of finction load_STL
+
+def load_STL_ASCII (file):
+    """Load a facet from an STL file"""
+    # List of points which make up each facet
+    facetList  = []
+    # Read the first line and check if it says solid
+    line = file.readlines(1)[0]
+    if line.startswith("solid") == False:
+        print("File is bad - expected solid")
         return 1
-
+    # Loop through the entire file, loading in facets
     for line in file:
-    
+        # Clear out the whitespace
         line = line.rstrip().lstrip()
-        #print(line)
-
+        # If its the start of a facet:
         if line.startswith("facet normal "):
-            # Start of a facet
+            # Find the normal:
             normal = [0, 0, 0]
-
-            #print("load the normal")
             line = line.lstrip("facet normal ")
             for i, val in enumerate(line.split()):
                 try:
                     normal[i] = float(val)
-                    #print(float(val), "is float number ", i)
-                    
-                except:
+                except ValueError:
                     print(val, "is not a float")
-               
-            #Find the coords
+                    return 1
+            
+            #Find the coords:
             coords = []
             line = file.readline().rstrip().lstrip()
-
             if  line == "outer loop":
                 for i in range(3):
                     line = file.readline().rstrip().lstrip()
-                    #print(line)
-
                     if line.startswith("vertex "):
                         line = line.lstrip("vertex ")
                         point = ()
                         for val in line.split():
                             try:
                                 point = point + (float(val),)
-                                #print(float(val), "is float number ", j)
-                    
                             except ValueError:
                                 print(val, "is not a float")
                         coords.append(point)
-
                     else:
                         print("File is bad - expected vertex")
                         return 1
-                    
-                # check if the closing lines are right
+                
+                # Check if the closing lines of each facet are correct
                 line = file.readline().rstrip().lstrip()
                 if line != "endloop":
                     print("File is bad - expected endloop")
@@ -79,49 +99,60 @@ def load_STL_ASCII (fileName, sliceDepth):
                 # if we are still in the loop, we have the facet data so process here
 
                 print("Facet loaded with normal", normal, "\n\tand coords", coords)
-
-                facetPoints = slice_facet(coords, normal, sliceDepth)
-
-                print("Slicer retuned:", facetPoints)
-
-                try:
-                    list(facetPoints)
-                    pointsList.extend(facetPoints)
-                except TypeError:
-                    print("No points")
-
-                print("\n--------------------")
-
-                
+                facetList.append(coords)
+               
             else:
                 print("File is bad - expected outer loop")
                 return 1
             
         elif line == "endsolid":
-            # end of file - stop here
-            file.close()
-            try:
-                pointsSet = list(set(pointsList))
-                print("Set of points:", pointsSet)
-                return pointsSet
-            
-            except:
-                print("List of points:", pointsList)
-                return pointsList
-            
+            # end of file - stop here ------------------------------------------------------
+            return facetList      
         else:
             print("File is bad - expected facet normal")
             return 1
-
-        #file.read() #should drop out of for loop as EOF
-
-    file.close()
-    print("Maybe file is binary?")
+    print("Dropped out of the loop - something went wrong!")
     return 1
 # End of function load_STL_ASCII
 
+def load_STL_Binary (file):
+    """Load a facet from an STL file"""
+    # List of points which make up each facet
+    facetList  = []
+    # Read the 80 byte ASCII header
+    header = struct.unpack('80s', file.read(80))[0].decode('utf-8').rstrip()
+    print("File Header:", header)
+    # Number of facets to find
+    facetCount = struct.unpack('<I', file.read(4))[0]
+    
+    # Loop through the entire file, loading in facets
+    for x in range(facetCount):
+        # Find the normal:
+        normal = [0,0,0]
+        for i in range(3):
+            normal[i] = struct.unpack('<f', file.read(4))[0]
+       
+        #Find the coords:
+        coords = [0,0,0]
+        for i in range(3):
+            point = ()
+            for j in range(3):
+                point = point + (struct.unpack('<f', file.read(4))[0],)
+            coords[i] = point
 
-def slice_facet(facetPoints, facetNormal, sliceDepth):
+        # Read the attribute byte count
+        attribute = struct.unpack('<H', file.read(2))[0]
+        if attribute != 0:
+            print("File bad - attribute not zero")
+            return 1
+        # Got the coordinates
+        print("Facet loaded with normal", normal, "\n\tand coords", coords)
+        facetList.append(coords)
+    return facetList
+# End of function load_STL_Binary
+
+
+def slice_facet(facetPoints, sliceDepth):
     """Slice a facet from an STL file to find points that intersect the slice plane"""
  
     # find whether the facet intersects the slice
@@ -230,7 +261,6 @@ def find_intersection(linePoints, sliceDepth):
 def dot(a, b):
     return sum(i*j for (i,j) in zip(a,b))
 
-
 def plot(pointsList, scale = 10, margin = 5):
     
     root = Tk()
@@ -249,15 +279,22 @@ def plot(pointsList, scale = 10, margin = 5):
         print ("An error has occured!")
 
     root.mainloop()
-    
+# End of function plot
+
 
 def main():
 
-    #print(find_intersection([[0,0,0],[2,2,2]],1))
-
-    
-    plot(load_STL_ASCII("2cm Cube ASCII.STL", 10))
- 
+    facetPoints = load_STL("2cm Coarse Ball Binary.STL")
+    pointsSet = []
+    for facet in facetPoints:
+        print("---------------")
+        try:
+            pointsSet.extend(slice_facet(facet, 10))
+        except:
+            pass
+    pointsSet = list(set(pointsSet))
+    print(pointsSet)
+    plot(pointsSet)
     
 main()
     
