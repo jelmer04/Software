218345859bfb6f46db989c2269bb9d42545ac7d7
jelmer04 <jon.s.elmer@googlemail.com>
2014-08-05 14:11:49 +0100

    menu with top level grid

--- Menu/main.py
+++ Menu/main.py
@@ -9,31 +9,35 @@ def calculate(*args):
         pass
     
 root = Tk()
-#root.overrideredirect(True)
-
-style = ttk.Style()
-style.theme_use("clam")
-style.configure('TButton', foreground='#334353')
-style.configure('TFrame', background='#334353')
-
+root.overrideredirect(True)
 root.title("Feet to Meters")
 root.geometry("320x240+450+350")
-root.grid_columnconfigure(0, minsize=320)
-root.grid_rowconfigure(0, minsize=40)
-root.grid_rowconfigure(1, minsize=200)
 
-titleframe = ttk.Frame(root)
+style = ttk.Style()
+style.theme_use("default")
+style.configure("TButton", background="#00ffff")
+style.configure("TFrame", background="#ff0000")
+style.configure("TLabel", background="#00ff00")
+style.configure("Title.TLabel", background="#ffff00", font="helvetica 24", padding="10 0 0 0")
+
+rootframe = ttk.Frame(root)
+rootframe.grid(row=0, column=0)
+
+rootframe.grid_columnconfigure(0, minsize=320)
+rootframe.grid_rowconfigure(0, minsize=40)
+rootframe.grid_rowconfigure(1, minsize=200)
+
+titleframe = ttk.Frame(rootframe)
 titleframe.grid(column=0, row=0, sticky=(N, E, W))
 titleframe.columnconfigure(0, minsize=20)
 titleframe.rowconfigure(0, minsize=40)
 backimage = PhotoImage(file="back.png")
-ttk.Button(titleframe, image=backimage, command=quit).grid(row=0, column=0)
-ttk.Label(titleframe, text="Menu Title").grid(row=0, column=1, sticky=(N, E, S, W))
+ttk.Button(titleframe, image=backimage, command=quit).grid(row=0, column=0, sticky=(N, E, S, W))
+ttk.Label(titleframe, text="Menu Title", style="Title.TLabel").grid(row=0, column=1, sticky="news")
 
-mainframe = ttk.Frame(root, padding="0")
+mainframe = ttk.Frame(rootframe, padding="0")
 mainframe.grid(column=0, row=1, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
+
 
 feet = StringVar()
 meters = StringVar()
@@ -50,6 +54,6 @@ ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
 
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
-root.bind('<Key-Escape>', quit)
+root.bind("<Key-Escape>", quit)
 
 root.mainloop()
