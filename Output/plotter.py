20354248c34dd34b1f3f5a2490136630184aace4
jelmer04 <jon.s.elmer@googlemail.com>
2014-09-03 10:04:10 +0100

    Commented output functions

--- Output/plotter.py
+++ Output/plotter.py
@@ -1,9 +1,8 @@
-from decimal import Decimal
 from tkinter import *
 
-scale = 40  # 40 for 2cm, 5 for 15cm
-margin = 10
-window = Decimal(0)
+# Defaults
+scale = 40              # 40 for 2cm, 5 for 15cm
+margin = 10             # Margin between plot and window border
 
 
 def graph(title="", scale=scale):
@@ -13,32 +12,36 @@ def graph(title="", scale=scale):
     @return:    graph canvas
     """
 
+    # Create TK environment
     root = Tk()
     root.title("Plot: {}".format(title))
     root.attributes('-alpha', 0.8)
     root.geometry('+10+10')
 
+    # Bind escape to close window
     def close(*args):
         root.destroy()
-
     root.bind("<Key-Escape>", close)
 
+    # Create canvas for drawing on
     canvas = Canvas(root, width=820, height=820, bg="white")
     canvas.pack()
 
+    # Create the grid and axes ticks
     for i in range(int(800 / scale) + 1):
+        # Grid
         canvas.create_line(i * scale + 10, 10, i * scale + 10, 810, width=1, fill="gray85")
         canvas.create_line(10, i * scale + 10, 810, i * scale + 10, width=1, fill="gray85")
 
+        # Ticks
         canvas.create_line(5, i * scale + 10, 10, i * scale + 10, width=1, fill="gray")
         canvas.create_line(i * scale + 10, 810, i * scale + 10, 815, width=1, fill="gray")
 
+    # Create axes lines
     canvas.create_line(10, 5, 10, 815, width=1, fill="gray")
     canvas.create_line(5, 810, 815, 810, width=1, fill="gray")
 
     return canvas
-
-
 # End of function graph()
 
 
@@ -57,63 +60,84 @@ def plot(canvas, linelist, trace="black", marker="", radius=0, numbers=False, ar
     @return:            none
     """
 
+    # Plot each line
     for i, line in enumerate(linelist):
-        x = (scale * line[1][0] + margin + ((window) * 800), scale * line[2][0] + margin + ((window) * 800))
-        y = (
-            800 - scale * line[1][1] + margin + ((-window) * 800), 800 - scale * line[2][1] + margin + ((-window) * 800))
+        # Scale the coordinates
+        x = (scale * line[1][0] + margin, scale * line[2][0] + margin)
+        y = (800 - scale * line[1][1] + margin, 800 - scale * line[2][1] + margin)
 
+        # Start markers
         fill = ""
-
         outline = marker
+        # If at first point, fill marker in green and outline in green
         if i == 0:
             outline = "green"
             fill = "green"
-
+        # Draw the marker
         canvas.create_oval(x[0] - radius, y[0] - radius, x[0] + radius, y[0] + radius, width=1, fill=fill,
                            outline=outline)
 
+        # End marker
         outline = marker
+        # If at last point, outline in red
         if line == linelist[-1]:
             outline = "red"
-
+        # Draw the marker
         canvas.create_oval(x[1] - radius, y[1] - radius, x[1] + radius, y[1] + radius, width=1, fill="",
                            outline=outline)
+
+        # Traces
+        # If trace colour starts with -- make trace dashed
         if trace.startswith("--"):
             fill = trace
             fill = fill.strip("-")
+            # Create a dashed trace
             canvas.create_line(x[0], y[0], x[1], y[1], width=1, fill=fill, dash=1)
         else:
+            # Create solid trace
             canvas.create_line(x[0], y[0], x[1], y[1], width=1, fill=trace, arrow=arrow)
 
+        # If numbers are to be drawn
         if numbers:
+            # Generate default coordinates for lines with no normal
             if not line[0]:
                 line[0] = [0, 0]
                 pass
             try:
+                # Try to draw number at normal position
                 canvas.create_text((x[0] + x[1]) / 2 + (line[0][0] * 10), (y[0] + y[1]) / 2 - (line[0][1] * 10), text=i,
                                    font="Helvetica 8")
             except:
+                # The normal coordinates were not right
                 # print("Problem with", i)
                 pass
     return
-
-
 # End of function plot()
 
 
 def points(canvas, linelist, radius=4, marker="blue", scale=scale):
-    scale = 40
-    margin = 10
+    """
+    Draw points at the start and end point of each line
 
-    print("Plotting:", [[x, y] for (n, x, y) in linelist])
+    :param canvas:      canvas to draw on
+    :param linelist:    list of lines to draw
+    :param radius:      radius of vertex markers
+    :param marker:      colour of vertex markers
+    :param scale:       scale of graph
+    :return:            none
+    """
+
+    # Plot markers for each line
     for i, line in enumerate(linelist):
-        x = (scale * line[1][0] + margin + ((1 - window) * 800), scale * line[2][0] + margin + ((1 - window) * 800))
-        y = (800 - scale * line[1][1] + margin + ((window) * 800), 800 - scale * line[2][1] + margin + ((window) * 800))
+        # Scale the coordinates
+        x = (scale * line[1][0] + margin, scale * line[2][0] + margin)
+        y = (800 - scale * line[1][1] + margin, 800 - scale * line[2][1] + margin)
 
+        # Draw the start marker
         canvas.create_oval(x[0] - radius, y[0] - radius, x[0] + radius, y[0] + radius, width=1, fill="",
                            outline=marker)
-
+        # Draw the end marker
         canvas.create_oval(x[1] - radius, y[1] - radius, x[1] + radius, y[1] + radius, width=1, fill="",
                            outline=marker)
-
     return
+# End of function points()
--- Output/post.py
+++ Output/post.py
@@ -1,7 +1,7 @@
 '''
 Write in the format:
 
-        [A,B,C,D,E]
+        A,B,C,D,E,
 
             A = x axis position
             B = y axis position
@@ -13,36 +13,67 @@ Write in the format:
 
 
 def newfile(filename):
+    """
+    Create a new empty file
+
+    :param filename: file to create
+    """
+    # Open the new file for writing
     with open(filename, "w") as file:
         pass
+# End of function newfile()
 
 
 def path(filename, path):
+    """
+    Write a path to the file
+
+    :param filename:    file name to write
+    :param path:        list of lines to write
+    """
+
+    # If the line is not empty:
     if len(path) > 0:
-
-        #for l in path:
-            #print("From {} to {}".format(l[1], l[2]))
-
+        # Open the file for appending
         with open(filename, "a") as file:
+            # Define format string
             write = "{:.2f},{:.2f},{:d},{:d},{:d},\n"
 
-            last = path[0]
-            # First point in a line
-            file.write(write.format(float(last[1][0]), float(last[1][1]), 0, 0, 0))
+            # Find the first point
+            first = path[0]
+            # Write the first point with "no extruding" option
+            file.write(write.format(float(first[1][0]), float(first[1][1]), 0, 0, 0))
 
+            # For each line in the path
             for i, line in enumerate(path):
+                # If line isn't a repeated point
                 if True or (line[1][0] != line[2][0]) and (line[1][1] != line[2][1]):
+
+                    # If the line is somewhere in the middle of the list write it with "extruding" option
                     if i < len(path) - 1:
                         file.write(write.format(float(line[2][0]), float(line[2][1]), 1, 0, 0))
+
+                    # If the line is the last of the path, write it with "extruding" and "end of island" options
                     else:
                         file.write(write.format(float(line[2][0]), float(line[2][1]), 1, 1, 0))
+# End of function path()
 
 
 def endlayer(filename):
+    """
+    Change the last line of the file to set the "end of layer" option
+
+    :param filename: file name to write to
+    """
+
+    # Open file for reading, and read final ine
     with open(filename, "r") as file:
         lines = file.readlines()
 
+    # Modify final line to include "end of layer" option
     lines[-1] = lines[-1][:-3] + "1,\n"
 
+    # Open file and write modified line back to file
     with open(filename, "w") as file:
         file.writelines(lines)
+# End of function endlayer()
